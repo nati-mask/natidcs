f65658b848a886b217f0edc57e7ba572fd9e29f2
Nati Mask <natimask@gmail.com>
2025-05-22 23:26:44 +0300

    Change set to dict set

--- nati-utilities.lua
+++ nati-utilities.lua
@@ -1,23 +1,59 @@
-local natils = {}
+--[[
+## Nati's additions to DCS
+]]
+Natils = Natils or {}
 
-natils.createSet = function (setName)
-    print('This will be set!')
-    return {
-        name = setName or 'unknown',
-        things = {},
-        addOne = function(self, thing)
-            if not (self and thing) then error("Don't call 'addOne' statically") end
-            print('Adding to ' .. self.name .. '...')
-            self.things[#self.things + 1] = thing
-            print('On '.. self.name ..' Now we are: '..table.concat(self.things, ', '))
+do
+
+    local includesKey = function (t, key)
+        for k in pairs(t) do
+            if (k == key) then return true end
         end
-    }
+        return false
+    end
+
+    local createDictSet = function (setName)
+        return {
+            name = setName or 'unknown',
+            dict = {},
+            add = function(self, key, val)
+                if not key then return end
+                if (type(key) ~= 'string') then error('Set key can only be a string') end
+                if includesKey(self.dict, key) then return end
+                self.dict[key] = val or 'unknown'
+            end,
+            has = function(self, key)
+                if not key then return false end
+                if (type(key) ~= 'string') then error('You can only check for string key in the set') end
+                return includesKey(self.dict, key)
+            end,
+            get = function(self, key)
+                return self.dict[key]
+            end,
+            concat = function(self, sep)
+                local text = ''
+                local aSep = sep or '\n'
+                local aSepLen = string.len(aSep)
+                for k,val in pairs(self.dict) do
+                    text = text..k..': '..val..aSep
+                end
+                return string.sub(text, 1, -1 - aSepLen)
+            end
+        }
+    end
+
+    Natils.createDictSet = createDictSet
+
 end
 
-local set1 = natils.createSet('Set 1')
-local set2 = natils.createSet('Set 2')
+-- local set1 = Natils.createDictSet('Testy')
 
-set1:addOne('PurrPurr')
-set1:addOne('Plaffie')
+-- print(set1:has('a'));
+-- set1:add('a', 'moshiko')
+-- set1:add('b', 'tamam')
+-- set1:add('b', 'keif')
+-- print(set1:has('a'));
 
-set2:addOne('Just')
+-- print(set1:get('b'));
+-- print(set1:get('c'));
+-- print('\n'..set1.name..' set now is:\n'..set1:concat())
