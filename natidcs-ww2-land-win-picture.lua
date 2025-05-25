92d1ff7612e474f3049b239a5768e19a9dc3bc2b
Nati Mask <natimask@gmail.com>
2025-05-25 19:33:36 +0300

    More debug, show lander

--- natidcs-ww2-land-win-picture.lua
+++ natidcs-ww2-land-win-picture.lua
@@ -10,6 +10,8 @@ do
         flag = nil,
         winPictureSet = nil,
         onLandListener = nil,
+        airbase = nil,
+        showLander = nil,
     }
 
     local textToBlue = function (text, seconds)
@@ -28,6 +30,14 @@ do
         end
     end
 
+    local displayWinningSet = function ()
+        if (not natidcs.ww2LandWinPicture.winPictureSet or natidcs.ww2LandWinPicture.winPictureSet:length() == 0) then
+            textToBlue('WW2 Winning set is empty', 20)
+        else
+            textToBlue('WW2 Winning set is:\n'..natidcs.ww2LandWinPicture.winPictureSet:concat(), 45)
+        end
+    end
+
     local onLand = function (event)
         if
             event.id == world.event.S_EVENT_LAND
@@ -40,10 +50,12 @@ do
             local playerName = landingUnit:getPlayerName()
             local unitName = landingUnit:getName()
             local airBaseName = event.place and event.place:getName();
+
             if not natidcs.ww2LandWinPicture.debug and not playerName then return end
+            if natidcs.ww2LandWinPicture.airbase and (not natidcs.ww2LandWinPicture.airbase == airBaseName) then return end
 
             if natidcs.ww2LandWinPicture.winPictureSet:has(getUnitUniqueId(landingUnit)) then
-                if natidcs.ww2LandWinPicture.debug then
+                if natidcs.ww2LandWinPicture.debug or natidcs.ww2LandWinPicture.showLander then
                     textToBlue((playerName and 'Player ' or 'Unit ')..(playerName or unitName)..' landed successfully at '..airBaseName..' and we are winning the day!', 60)
                 end
 
@@ -62,15 +74,17 @@ do
 
     end
 
-    local addBluePlanesToWinPicture = function (flag, options)
+    local takeWinPicture = function (flag, options)
         if not Natils then error('utilites for WW2 winning landing script was not loaded') end
-        if (not flag) or (type(flag) ~= 'number') then error('flag has not be set and be a number') end
+        if (not flag) or (type(flag) ~= 'number') then error('missing flag argument or it\'s not a number') end
 
         local zoneName = 'WW2_WIN_LAND_PICTURE'
 
         if (options and type(options) == 'table') then
             if (not options.debug) then natidcs.ww2LandWinPicture.debug = false end
-            if (options.zoneName and type(zoneName) == 'string') then zoneName = options.zoneName end
+            if (options.zoneName and type(options.zoneName) == 'string') then zoneName = options.zoneName end
+            if (options.airbase and type(options.airbase) == 'string') then natidcs.ww2LandWinPicture.airbase = options.airbase end
+            if (options.showLander and type(options.showLander) == 'boolean') then natidcs.ww2LandWinPicture.showLander = options.showLander end
         end
 
         local zone = trigger.misc.getZone(zoneName)
@@ -84,20 +98,17 @@ do
 
         natidcs.ww2LandWinPicture.flag = flag
 
+        if natidcs.ww2LandWinPicture.debug then
+            displayWinningSet()
+            textToBlue('Winning configurations:\n'..mist.utils.tableShow(natidcs.ww2LandWinPicture), 45)
+        end
+
         addOnLandEventListener()
 
     end
 
-    local displayWinningSet = function ()
-        if (not natidcs.ww2LandWinPicture.winPictureSet or natidcs.ww2LandWinPicture.winPictureSet:length() == 0) then
-            textToBlue('WW2 Winning set is empty', 20)
-        else
-            textToBlue('WW2 Winning set is:\n'..natidcs.ww2LandWinPicture.winPictureSet:concat(), 45)
-        end
-    end
-
     -- Exports:
-    natidcs.ww2LandWinPicture.addBluePlanesToPicture = addBluePlanesToWinPicture
+    natidcs.ww2LandWinPicture.takeWinPicture = takeWinPicture
     natidcs.ww2LandWinPicture.displayWinningSet = displayWinningSet
 
 end
